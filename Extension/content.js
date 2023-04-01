d34e7117e633a6281920d6344c2505831e35dac0
Arindam <arindambhowal10@gmail.com>
2023-04-01 15:13:09 +0530

    added snapshot and certificate functionalaties

--- Extension/content.js
+++ Extension/content.js
@@ -12,17 +12,33 @@
     // });
   };
 
+
+  const applyCertificate = async () => {
+    const currentTime = youtubePlayer.currentTime;
+    const duration = youtubePlayer.duration
+    let percentage = (currentTime/duration)*100 
+    console.log(percentage)
+    const postData = {
+      url: videoURL,
+      title: document.getElementsByClassName("ytp-title-note-box")[0].value,
+      completed: percentage
+    };
+
+    chrome.runtime.sendMessage({ type: "post_certificate", data: postData });
+
+  }
+
   const addNewBookmarkEventHandler = async () => {
     const currentTime = youtubePlayer.currentTime;
     const postData = {
       url: videoURL,
       timestamped: "Bookmark at " + getTime(currentTime),
-      currentTime : currentTime,
-      Topic : "",
+      currentTime: currentTime,
+      Topic: "",
       title: document.getElementsByClassName("ytp-title-note-box")[0].value,
       content: document.getElementsByClassName("ytp-note-box")[0].value,
-      copied: ""
-    //   snaped: "",
+      copied: "",
+      //   snaped: "",
     };
 
     // currentVideoBookmarks = await fetchBookmarks();
@@ -53,15 +69,24 @@
       TitleBox.setAttribute("value", "Enter Title");
       TitleBox.className = "ytp-title-note-box";
 
-
+      const certificateBtn = document.createElement("INPUT");
+      certificateBtn.setAttribute("type", "button");
+      certificateBtn.setAttribute("value", "Apply Certificate");
+      certificateBtn.className = "ytp-certificate-note-box";
+      certificateBtn.style.backgroundColor = 'dodgerblue';
+      certificateBtn.style.color = 'white';
 
       youtubeLeftControls =
         document.getElementsByClassName("ytp-left-controls")[0];
       youtubePlayer = document.getElementsByClassName("video-stream")[0];
 
+      youtubeLeftControls.appendChild(certificateBtn);
       youtubeLeftControls.appendChild(TitleBox);
       youtubeLeftControls.appendChild(noteBox);
       youtubeLeftControls.appendChild(bookmarkBtn);
+
+
+      certificateBtn.addEventListener("click", applyCertificate)
 
       bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
     }
@@ -76,7 +101,7 @@
       newVideoLoaded();
     } else if (type === "PLAY") {
       youtubePlayer.currentTime = value;
-    } 
+    }
     // else if (type === "DELETE") {
     //   currentVideoBookmarks = currentVideoBookmarks.filter(
     //     (b) => b.time != value
@@ -104,3 +129,140 @@ const getTime = (t) => {
 // function stopPropagation(e) {
 //   e.stopPropagation();
 // }
+
+var screenshotKey = false;
+var playbackSpeedButtons = false;
+var screenshotFunctionality = 0;
+var screenshotFormat = "png";
+var extension = "png";
+
+function CaptureScreenshot() {
+  var appendixTitle = "Capture_file." + extension;
+
+  var title;
+
+  var headerEls = document.querySelectorAll(
+    "h1.title.ytd-video-primary-info-renderer"
+  );
+
+  function SetTitle() {
+    if (headerEls.length > 0) {
+      title = headerEls[0].innerText.trim();
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  if (SetTitle() == false) {
+    headerEls = document.querySelectorAll("h1.watch-title-container");
+
+    if (SetTitle() == false) title = "";
+  }
+
+  var player = document.getElementsByClassName("video-stream")[0];
+
+  var time = player.currentTime;
+
+  title += " ";
+
+  let minutes = Math.floor(time / 60);
+
+  time = Math.floor(time - minutes * 60);
+
+  if (minutes > 60) {
+    let hours = Math.floor(minutes / 60);
+    minutes -= hours * 60;
+    title += hours + "-";
+  }
+
+  title += minutes + "-" + time;
+
+  title += " " + appendixTitle;
+
+  var canvas = document.createElement("canvas");
+  canvas.width = player.videoWidth;
+  canvas.height = player.videoHeight;
+  canvas.getContext("2d").drawImage(player, 0, 0, canvas.width, canvas.height);
+
+  var downloadLink = document.createElement("a");
+  downloadLink.download = title;
+
+  function DownloadBlob(blob) {
+    downloadLink.href = URL.createObjectURL(blob);
+    downloadLink.click();
+  }
+
+  async function ClipboardBlob(blob) {
+    const clipboardItemInput = new ClipboardItem({ "image/png": blob });
+    await navigator.clipboard.write([clipboardItemInput]);
+  }
+
+  // If clipboard copy is needed generate png (clipboard only supports png)
+  if (screenshotFunctionality == 1 || screenshotFunctionality == 2) {
+    canvas.toBlob(async function (blob) {
+      await ClipboardBlob(blob);
+      // Also download it if it's needed and it's in the correct format
+      if (screenshotFunctionality == 2 && screenshotFormat === "png") {
+        DownloadBlob(blob);
+      }
+    }, "image/png");
+  }
+
+  // Create and download image in the selected format if needed
+  if (
+    screenshotFunctionality == 0 ||
+    (screenshotFunctionality == 2 && screenshotFormat !== "png")
+  ) {
+    canvas.toBlob(async function (blob) {
+      DownloadBlob(blob);
+    }, "image/" + screenshotFormat);
+  }
+}
+
+function AddScreenshotButton() {
+  var ytpRightControls =
+    document.getElementsByClassName("ytp-right-controls")[0];
+  if (ytpRightControls) {
+    ytpRightControls.prepend(screenshotButton);
+  }
+}
+
+var screenshotButton = document.createElement("button");
+screenshotButton.className = "screenshotButton ytp-button";
+screenshotButton.style.width = "auto";
+screenshotButton.innerHTML = "Screenshot";
+screenshotButton.style.cssFloat = "left";
+screenshotButton.onclick = CaptureScreenshot;
+
+chrome.storage.sync.get(
+  [
+    "screenshotKey",
+    "playbackSpeedButtons",
+    "screenshotFunctionality",
+    "screenshotFileFormat",
+  ],
+  function (result) {
+    screenshotKey = result.screenshotKey;
+    playbackSpeedButtons = result.playbackSpeedButtons;
+    if (result.screenshotFileFormat === undefined) {
+      screenshotFormat = "png";
+    } else {
+      screenshotFormat = result.screenshotFileFormat;
+    }
+
+    if (result.screenshotFunctionality === undefined) {
+      screenshotFunctionality = 0;
+    } else {
+      screenshotFunctionality = result.screenshotFunctionality;
+    }
+
+    if (screenshotFormat === "jpeg") {
+      extension = "jpg";
+    } else {
+      extension = screenshotFormat;
+    }
+  }
+);
+
+AddScreenshotButton();
